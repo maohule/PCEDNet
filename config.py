2c71cf88dcb5be2a01ae5d433e1195d10ac8e522
maohule <932604487@qq.com>
2023-11-25 19:57:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ __C.VIS = edict()
 
 #------------------------------DATA------------------------
 __C.DATA.STD_SIZE = (768,1024)
-#__C.DATA.DATA_PATH = '/home/wanglin/workspace/pytorch_DL/PCCNet_vgg_2/CC/' +str(__C.DATA.STD_SIZE[0]) + 'x' + str(__C.DATA.STD_SIZE[1]) + '/shanghaitech_part_B' 
 __C.DATA.DATA_PATH = 'path_to_the_datasets'          
 __C.DATA.MEAN_STD = 'dataset_MEAN_STD' 
 __C.DATA.DEN_ENLARGE = 1.
@@ -23,19 +22,17 @@ __C.DATA.DEN_ENLARGE = 1.
 __C.TRAIN.INPUT_SIZE = 'input_size'
 __C.TRAIN.SEED = 640
 __C.TRAIN.RESUME = ''#model path
-__C.TRAIN.BATCH_SIZE = 6 #imgs
+__C.TRAIN.BATCH_SIZE = 'BATCH_SIZE' #imgs
 __C.TRAIN.BCE_WIEGHT = 'BCE _WIEGHT_value'
 
 __C.TRAIN.SEG_LR = 'SEG_LR_value'
 __C.TRAIN.SEG_WIEGHT = 'SEG _WIEGHT_value'
 
-__C.TRAIN.NUM_BOX = 20 #boxes
-
 __C.TRAIN.GPU_ID = 'GPU_Device_number'
 
 # base lr
 __C.TRAIN.LR = 'TRAIN_LR'
-__C.TRAIN.LR_DECAY = 0.995
+__C.TRAIN.LR_DECAY = 'LR_DECAY'
 __C.TRAIN.NUM_EPOCH_LR_DECAY = 1 # epoches
 
 __C.TRAIN.MAX_EPOCH = 1000
